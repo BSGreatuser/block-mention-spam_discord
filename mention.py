9aed9435f2209a0c42e37db5479c7550afae92af
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2021-11-14 18:22:56 +0900

    Update mention.py

--- mention.py
+++ mention.py
@@ -6,15 +6,15 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-	if len(message.mentions) > 9:
-		if mesaage.author.guild_permissions.manage_messages:
-			return
+    if len(message.mentions) > 9:
+	if mesaage.author.guild_permissions.manage_messages:
+	    return
 			
-		try:
-			await message.delete()
-		except:
-			pass
-		await message.author.ban(reason='멘션테러')
-		await message.channel.send(f'{message.author} 차단됨')
+        try:
+            await message.delete()
+	except:
+            pass
+	await message.author.ban(reason='멘션테러')
+        await message.channel.send(f'{message.author} 차단됨')
 		
 client.run(token)
